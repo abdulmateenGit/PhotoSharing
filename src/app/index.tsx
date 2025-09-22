09301e3ea9b107866d8fb23161720fcfb64363d6
Abdul Mateen <abdulmateen.lhr00@gmail.com>
2025-09-22 09:27:20 +0500

    NativeWind Installation

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,8 +3,8 @@ import React from "react";
 
 export default function Home() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text> Home </Text>
+    <View className='flex-1 items-center justify-center bg-neutral-600'>
+      <Text className='text-2xl, font-bold, text-white'> Home </Text>
     </View>
   );
 }
