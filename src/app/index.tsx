aee98f179feb74b0f73f364596b492d0d1e01957
Abdul Mateen <abdulmateen.lhr00@gmail.com>
2025-09-28 21:45:53 +0500

    Supabase Setup

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,8 +1,16 @@
 import { View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Link } from "expo-router";
+import { useEffect } from "react";
+import { supabase } from "../lib/supabase";
 
 export default function Home() {
+  useEffect(() => {
+    supabase
+      .from("events")
+      .select("*, assets(*)")
+      .then((data) => console.log(JSON.stringify(data, null, 2)));
+  }, []);
   return (
     <View className="flex-1 items-center justify-center bg-neutral-600 gap-4">
       <Link href="/camera" className="text-white">
